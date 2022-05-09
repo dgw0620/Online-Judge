b0fb5f16d6209b5894d8328c3937036bfa667598
dgw0620 <105223542+dgw0620@users.noreply.github.com>
2022-05-10 00:01:16 +0900

    Update ABCDE.py

--- 백준/Gold/13023. ABCDE/ABCDE.py
+++ 백준/Gold/13023. ABCDE/ABCDE.py
@@ -1,6 +1,6 @@
 def dfs(v, visited):
     visited.append(v)
-    if len(visited) >= 5:
+    if len(visited) >= 5: # 노드의 깊이가 5 이상이면 True
         return True
     for w in graph[v]:
         if not w in visited:
@@ -12,15 +12,18 @@ def dfs(v, visited):
 n, m = map(int, input().split())
 graph = [[] for i in range(n)]
 
+# graph에 노드간 엣지 저장하기
 for i in range(m):
     n1, n2 = map(int, input().split())
     graph[n1].append(n2)
     graph[n2].append(n1)
 
+# 0번 노드부터 순서로 실행    
 for i in range(n):
     if dfs(i, []):
         print(1)
         exit(0)
+        
 print(0)
 exit(0)
 
