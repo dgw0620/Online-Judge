43faef443b952cbff6595ef3d0440dd4fc525f77
dgw0620 <105223542+dgw0620@users.noreply.github.com>
2022-05-28 17:27:28 +0900

    [Silver I] Title: 미로 탐색, Time: 104 ms, Memory: 32452 KB -BaekjoonHub

---
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -0,0 +1,28 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+def bfs(dq):
+    while dq:
+        y, x, cnt = dq.popleft()
+        # visited[y][x] = True
+        for d in dir:
+            yy = y + d[0]
+            xx = x + d[1]
+            if yy < 0 or xx < 0 or yy >= n or xx >= m or maze[yy][xx] != 1:
+                continue
+            elif yy == n - 1 and xx == m - 1:
+                print(cnt + 1)
+                exit(0)
+            else:
+                dq.append((yy, xx, cnt + 1))
+                maze[yy][xx] = -1
+
+n, m = map(int, input().split())
+maze = [list(map(int, input().strip())) for _ in range(n)]
+
+dir = [[-1, 0], [0, 1], [1, 0], [0, -1]]
+dq = deque([(0, 0, 1)])
+# visited = [[False] * m for _ in range(n)]
+
+bfs(dq)
