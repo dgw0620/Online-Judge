12ee54db51cef7649bc73842fdf364c803f4ce2c
dgw0620 <105223542+dgw0620@users.noreply.github.com>
2023-05-16 01:20:31 +0900

    [Bronze I] Title: 약수, Time: 208 ms, Memory: 17784 KB -BaekjoonHub

---
+++ 백준/Bronze/1037. 약수/약수.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        if (n == 1) {
+            System.out.println((int) Math.pow(sc.nextLong(), 2));
+        } else {
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+            Arrays.sort(arr);
+            System.out.println(arr[0] * arr[arr.length - 1]);
+        }
+    }
+}
