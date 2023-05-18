fe012c36dc5f2b2a1436e7e0a72654c68ca794f1
dgw0620 <105223542+dgw0620@users.noreply.github.com>
2023-05-18 23:19:03 +0900

    [Silver III] Title: N과 M (4), Time: 312 ms, Memory: 23940 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+    static Scanner sc = new Scanner(System.in);
+    static StringBuilder sb = new StringBuilder();
+
+    static int n = sc.nextInt();
+    static int m = sc.nextInt();
+
+    public static void solution(Stack<Integer> stack) {
+        if (stack.size() == m) {
+            for (int i : stack)
+                sb.append(i + " ");
+            sb.append("\n");
+            return;
+        }
+        for (int i = 1; i <= n; i++) {
+            if (stack.size() == 0 || stack.peek() <= i){
+                stack.push(i);
+                solution(stack);
+                stack.pop();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        solution(stack);
+        System.out.println(sb);
+    }
+}
