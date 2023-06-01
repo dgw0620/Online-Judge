a3817035927502fb7d8cb4b96d31927df0d44d5c
dgw0620 <105223542+dgw0620@users.noreply.github.com>
2023-06-01 16:10:43 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.02 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int num) {
+        int answer = 0;
+        long num2 = (long) num;
+        while (num2 != 1) {
+            if (answer > 500) return -1;
+            if (num2 % 2 == 0) {
+                num2 /= 2;
+            } else {
+                num2 = num2 * 3 + 1;  
+            }
+            answer++;
+        }
+        
+        return answer;
+    }
+}
