7840fbf2c35c6e8c43f731f40adc7a64a3f19566
dgw0620 <dgw0620@naver.com>
2023-07-26 02:36:43 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 321.65 ms, Memory: 94.9 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,19 +2,12 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        String answer = "";
-        
         Arrays.sort(participant);
         Arrays.sort(completion);
         
         for (int i = 0; i < completion.length; i++) {
-            if (!participant[i].equals(completion[i])) {
-                answer = participant[i];
-                break;
-            }
+            if (!participant[i].equals(completion[i])) return participant[i];
         }
-        if (answer.equals("")) answer = participant[participant.length - 1];
-        
-        return answer;
+        return participant[participant.length - 1];
     }
 }
