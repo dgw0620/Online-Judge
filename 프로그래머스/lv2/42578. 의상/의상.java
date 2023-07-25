80afae6a1366d340317b6d3a2c29e0e891d86d3f
dgw0620 <dgw0620@naver.com>
2023-07-26 04:26:30 +0900

    [level 2] Title: 의상, Time: 0.10 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42578. 의상/의상.java
+++ 프로그래머스/lv2/42578. 의상/의상.java
@@ -1,16 +1,15 @@
 import java.util.*;
 class Solution {
     public int solution(String[][] clothes) {
-        Map<String, Integer> map = new HashMap<>();
-        
+        HashMap<String, Integer> map = new HashMap<>();
         int answer = 1;
         
-        for (String[] s : clothes) {
-            map.put(s[1], map.getOrDefault(s[1], 0) + 1);
+        for (String[] c : clothes) {
+            map.put(c[1], map.getOrDefault(c[1], 0) + 1);
         }
         
-        for (int i : map.values()) {
-            answer *= i + 1;
+        for (String key : map.keySet()) {
+            answer *= map.get(key) + 1;
         }
         
         return answer - 1;
